83d7402c5e5255bac6737c0b6b0bd8dda9d5aa8a
Bruno Zhong <69879040+Brunozhon@users.noreply.github.com>
2021-12-01 21:07:27 -0500

    Update geometry-dash.js

--- geometry-dash.js
+++ geometry-dash.js
@@ -2,11 +2,17 @@ var geometryDash = {
   canvas: document.createElement("canvas"),
   ctx: null,
   player: null,
+  Player: function(name, color, x, y) {
+    this.name = name
+    this.color = color
+    this.x = x
+    this.y = y
+  },
   init: function() {
     document.body.append(this.canvas)
     this.ctx = this.canvas.getContext("2d")
   },
   start: function() {
-    
+    this.player = this.Player("Geometry Dash Player", "green", 0, 0)
   }
 }
